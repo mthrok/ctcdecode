fcef04f2d3ac11a224c5317056cc794aba3e81dd
moto <855818+mthrok@users.noreply.github.com>
2021-04-21 15:47:21 +0000

    merge test

--- tests/test_decode.py
+++ tests/test_decode.py
@@ -104,6 +104,61 @@ class TestDecoders(unittest.TestCase):
         self.assertEqual(''.join(result.label_sequences[0][0]), self.beam_search_result[0])
         self.assertEqual(''.join(result.label_sequences[1][0]), self.beam_search_result[1])
 
+    def test_decode_wav2vec2_sample():
+        WAV2VEC2_ENGLISH_LABEL = [
+            '<s>',
+            '<pad>',
+            '</s>',
+            '<unk>',
+            '|',
+            'E',
+            'T',
+            'A',
+            'O',
+            'N',
+            'I',
+            'H',
+            'S',
+            'R',
+            'D',
+            'L',
+            'U',
+            'M',
+            'W',
+            'C',
+            'F',
+            'G',
+            'Y',
+            'P',
+            'B',
+            'V',
+            'K',
+            "'",
+            'X',
+            'J',
+            'Q',
+            'Z',
+        ]
+        encoder_output = torch.load(
+            os.path.join(
+                os.path.dirname(__file__),
+                'librispeech-test-clean-121-121726-0000-with-wav2vec_small_960h.pt',
+            )
+        )
+        decoder = BeamSearchDecoder(
+            WAV2VEC2_ENGLISH_LABEL,
+            cutoff_top_n=40,
+            cutoff_prob=1.0,
+            beam_size=100,
+            num_processes=4,
+            blank_id=0,
+            is_nll=True,
+        )
+        results = decoder.decode(encoder_output)
+
+        transcript = "".join(results.label_sequences[0][0])
+        assert transcript == 'ALSO|A|POPULAR|CONTRIVANCE|WHEREBY|LOVE|MAKING|MAY|BE|SUSPENDED|BUT|NOT|STOPPED|DURING|THE|PICNIC|SEASON|'
+
 
 if __name__ == '__main__':
     unittest.main()
